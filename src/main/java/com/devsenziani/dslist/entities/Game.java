2b53ada89139c75623c6a25a6e44b7acbefc5a8c
Rafael Senziani <87246902+Senziani@users.noreply.github.com>
2023-07-11 00:34:24 -0300

    Game seed

--- src/main/java/com/devsenziani/dslist/entities/Game.java
+++ src/main/java/com/devsenziani/dslist/entities/Game.java
@@ -9,104 +9,134 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-
-
 //  Entity confira a classe do java para ser equivalente a uma tabela do banco relacional
 // @table para setar o nome da tabela
 @Entity
-@Table(name="tb_game")
+@Table(name = "tb_game")
 public class Game {
 	// vai representar o jogo
-	
-	
+
 	// @Id para o id ser auto incrementado
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String title;
-	
+
 	// customizando o nome no banco
-	@Column(name="game_year")
+	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	// caso nao coloque isso, ele trava como 256 caracteres a descrição
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
-	
-	//contrutor
-	
+
+	// contrutor
+
 	public Game() {
-		
+
 	}
+
 // botão direito >> source >> generate construtor using Filds para gerar ocontrutor
 // ja passando os argumento das variaveis
-	public Game(long id, String title, Integer year, String genre, String platform, String imgUrl,
+	public Game(long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		// o objeto = argumento
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
 	}
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public Integer getYear() {
 		return year;
 	}
+
 	public void setYear(Integer year) {
 		this.year = year;
 	}
+
 	public String getGenre() {
 		return genre;
 	}
+
 	public void setGenre(String genre) {
 		this.genre = genre;
 	}
-	public String getPlatform() {
-		return platform;
+
+	public String getPlatforms() {
+		return platforms;
 	}
-	public void setPlatform(String platform) {
-		this.platform = platform;
+
+	public Double getScore() {
+		return score;
 	}
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
+
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
+	}
+
 	public String getImgUrl() {
 		return imgUrl;
 	}
+
 	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
 	}
+
 	public String getShortDescription() {
 		return shortDescription;
 	}
+
 	public void setShortDescription(String shortDescription) {
 		this.shortDescription = shortDescription;
 	}
+
 	public String getLongDescription() {
 		return longDescription;
 	}
+
 	public void setLongDescription(String longDescription) {
 		this.longDescription = longDescription;
 	}
-	
+
 	// comparando os games // gerado automaticamente
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -118,14 +148,7 @@ public class Game {
 		Game other = (Game) obj;
 		return id == other.id;
 	}
-	
+
 // botão direito >> source >> generate geters and seters encapsulamento das variaveis
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
